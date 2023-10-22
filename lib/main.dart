9d88a54de73f87668dcaaa8d2f6cf7012be9b8bc
Fraser Nicholson <fraser.nicholson@btinternet.com>
2023-10-22 12:59:52 +0100

    Check for notification perms, fix buttons and change icon

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,22 @@ import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:overlay_support/overlay_support.dart';
+import 'package:permission_handler/permission_handler.dart';
 import 'pages/matches_page.dart';
 import 'pages/notifications_page.dart';
 
 void main() async {
   await dotenv.load(fileName: "lib/.env");
+
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+
+  await Permission.notification.isDenied.then((value) {
+    if (value) {
+      Permission.notification.request();
+    }
+  });
   await FirebaseMessaging.instance.setForegroundNotificationPresentationOptions(
       alert: true, badge: true, sound: true);
   runApp(const App());
@@ -51,78 +59,72 @@ class LandingPage extends StatelessWidget {
           Expanded(
             flex: 1,
             child: Center(
-              child: FractionallySizedBox(
-                widthFactor: 0.9,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.blue,
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pushNamed(context, '/matches');
-                        },
-                        child: const Icon(
-                          Icons.sports_cricket,
-                          size: 100.0,
-                          color: Colors.white,
-                        ),
+              child: GestureDetector(
+                  onTap: () => {Navigator.pushNamed(context, '/matches')},
+                  child: FractionallySizedBox(
+                    widthFactor: 0.9,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.blue,
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
-                      const SizedBox(height: 10.0),
-                      const Text(
-                        'Matches',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                        ),
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.sports_cricket,
+                            size: 100.0,
+                            color: Colors.white,
+                          ),
+                          SizedBox(height: 10.0),
+                          Text(
+                            'Matches',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                ),
-              ),
+                    ),
+                  )),
             ),
           ),
           const SizedBox(height: 20.0),
           Expanded(
             flex: 1,
             child: Center(
-              child: FractionallySizedBox(
-                widthFactor: 0.9,
-                child: Container(
-                  decoration: BoxDecoration(
-                    color: Colors.green,
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      GestureDetector(
-                        onTap: () {
-                          Navigator.pushNamed(context, '/notifications');
-                        },
-                        child: const Icon(
-                          Icons.notifications,
-                          size: 100.0,
-                          color: Colors.white,
-                        ),
+              child: GestureDetector(
+                  onTap: () => {Navigator.pushNamed(context, '/notifications')},
+                  child: FractionallySizedBox(
+                    widthFactor: 0.9,
+                    child: Container(
+                      decoration: BoxDecoration(
+                        color: Colors.green,
+                        borderRadius: BorderRadius.circular(10.0),
                       ),
-                      const SizedBox(height: 10.0),
-                      const Text(
-                        'Notifications',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                        ),
+                      child: const Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.notifications,
+                            size: 100.0,
+                            color: Colors.white,
+                          ),
+                          SizedBox(height: 10.0),
+                          Text(
+                            'Notifications',
+                            style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
                       ),
-                    ],
-                  ),
-                ),
-              ),
+                    ),
+                  )),
             ),
           ),
           const SizedBox(height: 20.0),
